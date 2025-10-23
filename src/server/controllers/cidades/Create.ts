28b2ca730aad799e534bb5fe008f7b115f8d2ce0
João Silva <118225101+JoaoGomesDev32@users.noreply.github.com>
2025-10-23 19:08:33 +0100

    feat: params the schema validation, upgrade function, return all errors one time

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -3,72 +3,28 @@ import { StatusCodes } from 'http-status-codes';
 import * as yup from 'yup';
 import { validation } from '../../shared/middleware';
 
-interface InterfaceCidade {
+interface ICidade {
   municipio: string;
   distrito: string;
 }
 
-const bodyValidation: yup.ObjectSchema<InterfaceCidade> = yup.object().shape({
-  municipio: yup.string().required().min(3),
-  distrito: yup.string().required().min(3),
-});
-
-export const createBodyValidator: RequestHandler = async (req, res, next) => {
-  try {
-    await bodyValidation.validate(req.body, { abortEarly: false });
-    return next();
-  } catch (err) {
-    const yupError = err as yup.ValidationError;
-    const errors: Record<string, string> = {};
-
-    yupError.inner.forEach((error) => {
-      if (error.path === undefined) return;
-      errors[error.path] = error.message;
-    });
-
-    return res.status(StatusCodes.BAD_REQUEST).json({ errors });
-  }
-};
-
-interface InterfaceFilter {
+interface IFilter {
   filter: string,
 }
 
-const queryValidation: yup.ObjectSchema<InterfaceFilter> = yup.object().shape({
-  filter: yup.string().required().min(3),
+export const createValidation = validation({
+  body: yup.object().shape({
+    municipio: yup.string().required().min(3),
+    distrito: yup.string().required().min(3),
+  }),
+  query: yup.object().shape({
+    filter: yup.string().required().min(3),
+  }),
 });
-
-export const createQueryValidator: RequestHandler = async (req, res, next) => {
-  try {
-    await queryValidation.validate(req.query, { abortEarly: false });
-    return next();
-  } catch (err) {
-    const yupError = err as yup.ValidationError;
-    const errors: Record<string, string> = {};
-
-    yupError.inner.forEach((error) => {
-      if (error.path === undefined) return;
-      errors[error.path] = error.message;
-    });
-
-    return res.status(StatusCodes.BAD_REQUEST).json({ errors });
-  }
-};
-
-
-
-
-
-
-
-
-
-
-export const createValidation = validation();
 
 
 export const create = async (
-  req: Request<{}, {}, InterfaceCidade>,
+  req: Request<{}, {}, ICidade>,
   res: Response
 ) => {
   console.log(req.body);
--- src/server/shared/middleware/Validation.ts
+++ src/server/shared/middleware/Validation.ts
@@ -1,23 +1,38 @@
 import { RequestHandler } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { ObjectSchema, ValidationError } from 'yup';
 
-type TValidation = () => RequestHandler;
+type TProperty = 'body' | 'header' | 'params' | 'query';
 
-export const validation: TValidation = () => async (req, res, next) => {
-  console.log('test');
+type TallSchemas = Record<TProperty, ObjectSchema<any>>;
 
-  try {
-      await queryValidation.validate(req.query, { abortEarly: false });
-      return next();
+type TValidation = (schemas: Partial<TallSchemas>) => RequestHandler;
+
+export const validation: TValidation = (schemas) => async (req, res, next) => {
+
+  const errorsResult: Record<string, Record<string, string>> = {};
+
+  Object.entries(schemas).forEach (([key, schema]) => {
+    
+    try {
+      schema.validateSync(req[key as TProperty], { abortEarly: false });
     } catch (err) {
-      const yupError = err as yup.ValidationError;
+      const yupError = err as ValidationError;
       const errors: Record<string, string> = {};
-  
+      
       yupError.inner.forEach((error) => {
         if (error.path === undefined) return;
         errors[error.path] = error.message;
       });
-  
-      return res.status(StatusCodes.BAD_REQUEST).json({ errors });
+      
+      errorsResult[key as TProperty] = errors;
+      
     }
+  });
 
+  if (Object.entries(errorsResult).length === 0) {
+    return next();
+  } else {
+    return res.status(StatusCodes.BAD_REQUEST).json({ errors: errorsResult });
+  } 
 };
