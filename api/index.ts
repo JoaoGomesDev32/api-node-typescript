b9b10a1fb3a4b6b8a86edf9a4dacefa5f7073952
João Silva <118225101+JoaoGomesDev32@users.noreply.github.com>
2025-11-03 17:43:47 +0000

    adjusts

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,12 @@
-import { server } from '../src/server/Server';
+import express from 'express';
+import 'dotenv/config';
+import '../src/server/shared/services/TranslationsYup';
+import { router } from '../src/server/routes';
+
+const server = express();
+
+server.use(express.json());
+server.use(router);
 
 export default server;
-
 
